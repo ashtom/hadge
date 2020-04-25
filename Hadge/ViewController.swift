f791cb7babb5e995935edd07dad54ecb8f89ffcc
Thomas Dohmke <thomas@dohmke.de>
2020-04-25 08:36:02 -0700

    Implement basic HealthKit flow.

--- Hadge/ViewController.swift
+++ Hadge/ViewController.swift
@@ -10,30 +10,25 @@ import UIKit
 import HealthKit
 
 class ViewController: UIViewController {
+    var healthStore: HKHealthStore?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        let healthStore = HKHealthStore()
-//        let objectTypes: Set<HKObjectType> = [
-//            HKObjectType.activitySummaryType()
-//        ]
-//
-//        healthStore.requestAuthorization(toShare: nil, read: objectTypes) { (_, _) in
-//            // Do something if the user didn't allow access
-//        }
-//
-//        let calendar = Calendar.autoupdatingCurrent
-//        var dateComponents = calendar.dateComponents([ .year, .month, .day ], from: Date())
-//        dateComponents.calendar = calendar
-//
-//        let predicate = HKQuery.predicateForActivitySummary(with: dateComponents)
-//        let query = HKActivitySummaryQuery(predicate: predicate) { (_, summaries, _) in
-//            guard let summaries = summaries, summaries.count > 0 else { return }
-//            print(summaries.first?.description ?? "")
-//        }
-//
-//        healthStore.execute(query)
+        healthStore = HKHealthStore()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        let objectTypes: Set<HKObjectType> = [
+            HKObjectType.activitySummaryType(),
+            HKObjectType.workoutType()
+        ]
+
+        healthStore?.requestAuthorization(toShare: nil, read: objectTypes) { (success, _) in
+            if success {
+                self.loadData()
+            }
+        }
     }
 
     @IBAction func signOut(_ sender: Any) {
@@ -41,5 +36,29 @@ class ViewController: UIViewController {
 
         let sceneDelegate = self.view.window?.windowScene?.delegate as? SceneDelegate
         sceneDelegate?.window?.rootViewController = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "LoginViewController")
+    }
+
+    func loadData() {
+        let calendar = Calendar.autoupdatingCurrent
+        var dateComponents = calendar.dateComponents([ .year, .month, .day ], from: Date())
+        dateComponents.calendar = calendar
+
+        let predicate = HKQuery.predicateForActivitySummary(with: dateComponents)
+        let activityQuery = HKActivitySummaryQuery(predicate: predicate) { (_, summaries, _) in
+            guard let summaries = summaries, summaries.count > 0 else { return }
+            print(summaries.first?.description ?? "")
+        }
+        healthStore?.execute(activityQuery)
+
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+        let sampleQuery = HKSampleQuery(
+            sampleType: .workoutType(),
+            predicate: nil,
+            limit: 0,
+            sortDescriptors: [sortDescriptor]) { (_, samples, _) in
+            guard let samples = samples, samples.count > 0 else { return }
+            print(samples.first?.description ?? "")
+        }
+        healthStore?.execute(sampleQuery)
     }
 }
