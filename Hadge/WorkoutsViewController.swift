1e7b47daa63a8f6cc57c238565b9eb4dbb34a390
Thomas Dohmke <thomas@dohmke.de>
2020-05-05 10:43:37 -0700

    Add viewWillAppear method.

--- Hadge/WorkoutsViewController.swift
+++ Hadge/WorkoutsViewController.swift
@@ -24,6 +24,10 @@ class WorkoutsViewController: UIViewController, UITableViewDataSource, UITableVi
         setUpRefreshControl()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
