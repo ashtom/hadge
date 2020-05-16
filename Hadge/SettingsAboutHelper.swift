ad6a7539dfad4b859e6b890371f75c7c8533a573
Thomas Dohmke <thomas@dohmke.de>
2020-05-16 08:27:19 -0700

    Implement link to repo.

--- Hadge/SettingsAboutHelper.swift
+++ Hadge/SettingsAboutHelper.swift
@@ -10,14 +10,15 @@ import UIKit
 
 class SettingsAboutHelper {
     func tableView(_ tableView: UITableView, cellForRow: Int) -> UITableViewCell {
-        let identifier = "LicenceCell"
+        let identifier = "AboutCell"
         let cell = tableView.dequeueReusableCell(withIdentifier: identifier) ?? UITableViewCell.init(style: .subtitle, reuseIdentifier: identifier)
         cell.separatorInset = UIEdgeInsets.init(top: 0, left: 15.0, bottom: 0, right: 0)
         cell.accessoryType = .disclosureIndicator
 
         switch cellForRow {
         case 0:
-            cell.textLabel?.text = "Licenses"
+            cell.accessoryView = UIImageView.init(image: UIImage.init(systemName: "link"))
+            cell.textLabel?.text = "Source Code"
         default:
             cell.textLabel?.text = "Undefined"
         }
@@ -27,6 +28,8 @@ class SettingsAboutHelper {
 
     func tableView(_ tableView: UITableView, didSelectRow: Int, viewController: SettingsViewController) {
         switch didSelectRow {
+        case 0:
+            UIApplication.shared.open(URL.init(string: "https://github.com/ashtom/hadge")!)
         default: // No op
             break
         }
